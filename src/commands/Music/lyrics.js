eae6d1ded88e682b5b912ad4601aa7209ca1c654
DevanAbinaya <97084324+DevanAbinaya@users.noreply.github.com>
2022-07-10 13:45:49 +0700

    Update lyrics.js

--- src/commands/Music/lyrics.js
+++ src/commands/Music/lyrics.js
@@ -1,5 +1,8 @@
 const { MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
+const config = require('../../config');
+const Genius = require('genius-lyrics');
+const Client = new Genius.Client(config.geniusAPI);
 
 module.exports = {
   name: "lyrics",
@@ -7,82 +10,111 @@ module.exports = {
   description: "Prints the lyrics of a song",
   permissions: [],
   usage: "lyrics <song name>",
-  player: true,
+  player: false,
   args: true,
   dj: false,
   inVoiceChannel: true,
   sameVoiceChannel: true,
 
   execute: async (message, args, client, prefix) => {
-    await message.reply({
-      embeds: [
-        new MessageEmbed()
-          .setColor(client.embedColor)
-          .setDescription("🔎 **Searching...**"),
-      ],
-    });
 
-    let player;
-    if (client.manager) {
-      player = client.manager.players.get(message.guild.id);
-    } else {
+    // let player;
+    // if (client.manager) {
+    //   player = client.manager.players.get(message.guild.id);
+    // } else {
+    //   return message.channel.send({
+    //     embeds: [
+    //       new MessageEmbed()
+    //         .setColor("RED")
+    //         .setDescription("Lavalink node is not connected"),
+    //     ],
+    //   });
+    // }
+// 
+    // if (!args && !player) {
+    //   return message.channel.send({
+    //     embeds: [
+    //       new MessageEmbed()
+    //         .setColor("RED")
+    //         .setDescription("There's nothing playing"),
+    //     ],
+    //   });
+    // }
+// 
+    // let search = args ? args : player.queue.current.title;
+// 
+    // const searches = await Client.songs.search(search);
+    // const firstSong = searches[0];
+    // let lyrics = await firstSong.lyrics();
+// 
+    // // // Lavalink api for lyrics
+    // // let url = `https://api.darrennathanael.com/lyrics?song=${search}`;
+// // 
+    // // let lyrics = await fetch(url)
+    // //   .then((res) => {
+    // //     return res.json();
+    // //   })
+    // //   .catch((err) => {
+    // //     return err.name;
+    // //   });
+    // 
+// 
+    // let text = lyrics.lyrics;
+    // let lyricsEmbed = new MessageEmbed()
+    //   .setColor(client.embedColor)
+    //   .setTitle(`${lyrics.full_title}`)
+    //   .setURL(lyrics.url)
+    //   .setThumbnail(lyrics.thumbnail)
+    //   .setDescription(text);
+
+    try{
+      const song = args.join(' ')
+      if (!song) return message.channel.send("Please provide a title of the song.")
+      const searches = await Client.songs.search(song);
+      const firstSong = searches[0];
+      let lyrics = await firstSong.lyrics();
+
+      if (!firstSong) {
+           return message.channel.send({
+             embeds: [
+               new MessageEmbed()
+                 .setColor("RED")
+                 .setDescription(
+                   `❌ | No lyrics found for ${song}!\nMake sure you typed in your search correctly.`
+                 ),
+             ],
+           });
+         }
+
+
+      let text = lyrics;
+      let lyricsEmbed = new MessageEmbed()
+        .setColor(client.embedColor)
+        .setTitle(`${firstSong.fullTitle}`)
+        .setURL(firstSong.url)
+        .setThumbnail(firstSong.thumbnail)
+        .setDescription(text);
+
+      if (text.length > 4096) {
+        text = text.substring(0, 4090) + "[...]";
+        lyricsEmbed
+          .setDescription(text)
+          .setFooter({ text: "Truncated, the lyrics were too long." });
+      }
+  
+      return message.channel.send({ embeds: [lyricsEmbed] });
+
+
+    } catch(e) {
       return message.channel.send({
         embeds: [
           new MessageEmbed()
-            .setColor("RED")
-            .setDescription("Lavalink node is not connected"),
-        ],
-      });
-    }
-
-    if (!args && !player) {
-      return message.channel.send({
-        embeds: [
-          new MessageEmbed()
-            .setColor("RED")
-            .setDescription("There's nothing playing"),
-        ],
-      });
-    }
-
-    let search = args ? args : player.queue.current.title;
-    // Lavalink api for lyrics
-    let url = `https://api.darrennathanael.com/lyrics?song=${search}`;
-
-    let lyrics = await fetch(url)
-      .then((res) => {
-        return res.json();
+          .setColor('RED')
+          .setDescription(`${e}`)
+        ]
       })
-      .catch((err) => {
-        return err.name;
-      });
-    if (!lyrics || lyrics.response !== 200 || lyrics === "FetchError") {
-      return message.channel.send({
-        embeds: [
-          new MessageEmbed()
-            .setColor("RED")
-            .setDescription(
-              `❌ | No lyrics found for ${search}!\nMake sure you typed in your search correctly.`
-            ),
-        ],
-      });
-    }
+  }
 
-    let text = lyrics.lyrics;
-    let lyricsEmbed = new MessageEmbed()
-      .setColor(client.embedColor)
-      .setTitle(`${lyrics.full_title}`)
-      .setURL(lyrics.url)
-      .setThumbnail(lyrics.thumbnail)
-      .setDescription(text);
-
-    if (text.length > 4096) {
-      text = text.substring(0, 4090) + "[...]";
-      lyricsEmbed
-        .setDescription(text)
-        .setFooter({ text: "Truncated, the lyrics were too long." });
-    }
-
-    return message.channel.send({ embeds: [lyricsEmbed] });
-  },
+    
+  }
 };
--- src/slashCommands/Music/lyrics.js
+++ src/slashCommands/Music/lyrics.js
@@ -1,11 +1,14 @@
-const { MessageEmbed } = require("discord.js");
+const { CommandInteraction, MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
+const config = require('../../config');
+const Genius = require('genius-lyrics');
+const Client = new Genius.Client(config.geniusAPI);
 
 module.exports = {
   name: "lyrics",
   description: "Prints the lyrics of a song",
   permissions: [],
-  player: true,
+  player: false,
   dj: false,
   inVoiceChannel: true,
   sameVoiceChannel: true,
@@ -22,71 +25,88 @@ module.exports = {
    *
    * @param {Client} client
    * @param {CommandInteraction} interaction
+   * @param {String[]} args 
    */
 
-  run: async (client, interaction) => {
+  run: async (client, interaction, args) => {
     await interaction.reply({
       embeds: [
         new MessageEmbed()
           .setColor(client.embedColor)
           .setDescription("🔎 **Searching...**"),
       ],
+      ephemeral: false
     });
+    
+    try{
+    
+    const arg = interaction.options.getString('song');
 
-    let player;
-    if (client.manager) {
-      player = client.manager.players.get(interaction.guild.id);
-    } else {
-      return interaction.reply({
-        embeds: [
-          new MessageEmbed()
-            .setColor("RED")
-            .setDescription("Lavalink node is not connected"),
-        ],
-      });
-    }
+    const searches = await Client.songs.search(arg);
+    const firstSong = searches[0];
+    const lyrics = await firstSong.lyrics();
 
-    const args = interaction.options.getString("song");
-    if (!args && !player) {
-      return interaction.editReply({
-        embeds: [
-          new MessageEmbed()
-            .setColor("RED")
-            .setDescription("There's nothing playing"),
-        ],
-      });
-    }
-
-    let search = args ? args : player.queue.current.title;
-    // Lavalink api for lyrics
-    let url = `https://api.darrennathanael.com/lyrics?song=${search}`;
-
-    let lyrics = await fetch(url)
-      .then((res) => {
-        return res.json();
-      })
-      .catch((err) => {
-        return err.name;
-      });
-    if (!lyrics || lyrics.response !== 200 || lyrics === "FetchError") {
-      return interaction.editReply({
-        embeds: [
-          new MessageEmbed()
-            .setColor("RED")
-            .setDescription(
-              `❌ | No lyrics found for ${search}!\nMake sure you typed in your search correctly.`
-            ),
-        ],
-      });
-    }
-
-    let text = lyrics.lyrics;
-    let lyricsEmbed = new MessageEmbed()
-      .setColor(client.embedColor)
-      .setTitle(`${lyrics.full_title}`)
-      .setURL(lyrics.url)
-      .setThumbnail(lyrics.thumbnail)
-      .setDescription(text);
+    // let player;
+    // if (client.manager) {
+    //   player = client.manager.players.get(interaction.guild.id);
+    // } else {
+    //   return interaction.reply({
+    //     embeds: [
+    //       new MessageEmbed()
+    //         .setColor("RED")
+    //         .setDescription("Lavalink node is not connected"),
+    //     ],
+    //   });
+    // }
+// 
+    // const args = interaction.options.getString("song");
+    // if (!args && !player) {
+    //   return interaction.editReply({
+    //     embeds: [
+    //       new MessageEmbed()
+    //         .setColor("RED")
+    //         .setDescription("There's nothing playing"),
+    //     ],
+    //   });
+    // }
+// 
+    // let search = args ? args : player.queue.current.title;
+    // // Lavalink api for lyrics
+    // let url = `https://api.darrennathanael.com/lyrics?song=${search}`;
+// 
+    // let lyrics = await fetch(url)
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .catch((err) => {
+    //     return err.name;
+    //   });
+    // if (!lyrics || lyrics.response !== 200 || lyrics === "FetchError") {
+    //   return interaction.editReply({
+    //     embeds: [
+    //       new MessageEmbed()
+    //         .setColor("RED")
+    //         .setDescription(
+    //           `❌ | No lyrics found for ${search}!\nMake sure you typed in your search correctly.`
+    //         ),
+    //     ],
+    //   });
+    // }
+// 
+    // let text = lyrics.lyrics;
+    // let lyricsEmbed = new MessageEmbed()
+    //   .setColor(client.embedColor)
+    //   .setTitle(`${lyrics.full_title}`)
+    //   .setURL(lyrics.url)
+    //   .setThumbnail(lyrics.thumbnail)
+    //   .setDescription(text);
+    let text = lyrics;
+      let lyricsEmbed = new MessageEmbed()
+        .setColor(client.embedColor)
+        .setTitle(`${firstSong.fullTitle}`)
+        .setURL(firstSong.url)
+        .setThumbnail(firstSong.thumbnail)
+        .setDescription(text);
 
     if (text.length > 4096) {
       text = text.substring(0, 4090) + "[...]";
@@ -96,5 +116,14 @@ module.exports = {
     }
 
     return interaction.editReply({ embeds: [lyricsEmbed] });
-  },
+   } catch(e) {
+     return interaction.channel.send({
+       embeds: [
+         new MessageEmbed()
+         .setColor('RED')
+         .setDescription(`${e}`)
+       ],
+     })
+ }
+}
 };
