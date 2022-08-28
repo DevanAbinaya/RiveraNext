48a4a7977709dc6fce70b5b6be8099827ed6b3f0
Factiven <97084324+DevanAbinaya@users.noreply.github.com>
2022-08-28 18:54:16 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,25 +2,25 @@ require("dotenv").config();
 
 module.exports = {
     token: process.env.TOKEN || "",  // your bot token
-    prefix: process.env.PREFIX || "!", // bot prefix
+    prefix: process.env.PREFIX || "-", // bot prefix
     geniusAPI: process.env.GENIUSAPI || "",
     ownerID: process.env.OWNERID || "", //your discord id
     SpotifyID: process.env.SPOTIFYID || "",
     SpotifySecret: process.env.SPOTIFYSECRET || "",
     mongourl: process.env.MONGO_URI || "", // MongoDb URL
-    embedColor: process.env.COlOR || "#303236", // embed colour
-    logs: process.env.LOGS || "875254787191504947", // channel id for guild create and delete logs
+    embedColor: process.env.COlOR || "#2f3136", // embed colour
+    logs: process.env.LOGS || "904628290000662560", // channel id for guild create and delete logs
     links: {
-        img: process.env.IMG || 'https://media.discordapp.net/attachments/963097935820750878/983300268131225651/20220606_145403.png', //setup system background image 
-        support: process.env.SUPPORT || 'https://discord.gg/ns8CTk9J3e', //support server invite link
-        invite: process.env.INVITE || 'https://discord.com/oauth2/authorize?client_id=977742811132743762&permissions=8&scope=bot%20applications.commands' //bot invite link
+        img: process.env.IMG || 'https://cdn.discordapp.com/attachments/986579286397964290/993392503778721792/2825704.gif', //setup system background image 
+        support: process.env.SUPPORT || 'https://discord.gg/v5fjSdKwr2', //support server invite link
+        invite: process.env.INVITE || 'https://discord.com/api/oauth2/authorize?client_id=991578084622991441&permissions=8&scope=applications.commands%20bot' //bot invite link
     },
     nodes: [
         {
-            host: process.env.NODE_HOST || "localhost",
-            identifier: process.env.NODE_ID || "local",
-            port: parseInt(process.env.NODE_PORT || "80"),
-            password: process.env.NODE_PASSWORD || "coders",
+            host: process.env.NODE_HOST || "lavalink.oops.wtf",
+            identifier: process.env.NODE_ID || "main",
+            port: parseInt(process.env.NODE_PORT || "2000"),
+            password: process.env.NODE_PASSWORD || "www.freelavalink.ga",
             secure: parseBoolean(process.env.NODE_SECURE || "false"),
 
         }
